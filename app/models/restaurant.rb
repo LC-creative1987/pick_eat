52dd1dd8392c69e95fd4e58933dc7b3cfdace2e8
Zohrab Mammadov <mammadov.zohrab@gmail.com>
2021-03-08 18:50:28 +0400

    New Reaturant form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant < ApplicationRecord
+  CUISINES = ["Lebanese", "Indian", "Italian", "Thai", "Chinese", "French"]
   belongs_to :user
   has_many :dishes, dependent: :destroy
   has_many :ingredients, dependent: :destroy
 
   validates_presence_of :name, :address, :phone_number, :cuisine
   validates :name, uniqueness: true
+  validates :cuisine, inclusion: { in: CUISINES }
 end
