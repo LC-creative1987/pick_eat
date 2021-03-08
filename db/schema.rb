3c69344ac4d7122af7c5253a33997ed960bf4343
Zohrab Mammadov <mammadov.zohrab@gmail.com>
2021-03-08 14:48:34 +0400

    Orders-models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_084852) do
+ActiveRecord::Schema.define(version: 2021_03_08_104607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_084852) do
 
   create_table "orders", force: :cascade do |t|
     t.datetime "date"
-    t.text "delivery_adress"
+    t.text "delivery_address"
     t.integer "eta"
     t.integer "total_price"
     t.string "status"
