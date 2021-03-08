5f5815c39f5ea0e30c6a914e31ba7ce3baa3d380
Octavia von Bernuth <octavia@bernuth.de>
2021-03-08 14:42:16 +0400

    Seed for restaurant and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_03_08_075621) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +71,6 @@ ActiveRecord::Schema.define(version: 2021_03_08_075621) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Destroying all restaurants, users..."
+Restaurant.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+5.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    password: "123456"
+    )
+end
+
+
+puts "Creating restaurants..."
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    cuisine: ["Lebanese", "Indian", "Italian", "Thai", "Chinese", "French"].sample,
+    user: User.all.sample
+    )
+end
