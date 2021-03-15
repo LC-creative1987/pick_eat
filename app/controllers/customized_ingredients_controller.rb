1afb6006a640381968ef45f2af6aeca1e6ff08b2
Octavia von Bernuth <octavia@bernuth.de>
2021-03-15 16:52:48 +0400

    customization

--- app/controllers/customized_ingredients_controller.rb
+++ app/controllers/customized_ingredients_controller.rb
@@ -2,34 +2,38 @@ class CustomizedIngredientsController < ApplicationController
   def increase_amount
     @customized_ingredient = CustomizedIngredient.find(params[:customized_ingredient_id])
     @ingredient = @customized_ingredient.dish_ingredient.ingredient
-    @customized_ingredient.quantity += @ingredient.change_increment
 
-    if @customized_ingredient.quantity <= @customized_ingredient.dish_ingredient.max_quantity
+    if @customized_ingredient.can_increase?
+      @customized_ingredient.quantity += @ingredient.change_increment
       @customized_ingredient.save
       respond_to do |format|
         format.html { redirect_to order_item_path(@customized_ingredient.order_item) }
-        format.js
+        format.js { render partial: "change_amount" }
       end
     else
-      flash[:notice] = "The value can not be bigger then the max"
-      redirect_to order_item_path(@customized_ingredient.order_item)
+      respond_to do |format|
+        format.html { redirect_to order_item_path(@customized_ingredient.order_item) }
+        format.js { render partial: "error" }
+      end
     end
   end
 
   def decrease_amount
     @customized_ingredient = CustomizedIngredient.find(params[:customized_ingredient_id])
     @ingredient = @customized_ingredient.dish_ingredient.ingredient
-    @customized_ingredient.quantity -= @ingredient.change_increment
 
-    if @customized_ingredient.quantity >= @customized_ingredient.dish_ingredient.min_quantity
+    if @customized_ingredient.can_decrease?
+      @customized_ingredient.quantity -= @ingredient.change_increment
       @customized_ingredient.save
       respond_to do |format|
         format.html { redirect_to order_item_path(@customized_ingredient.order_item) }
-        format.js
+        format.js { render partial: "change_amount" }
       end
     else
-      flash[:notice] = "The value can not be samller then then min"
-      redirect_to order_item_path(@customized_ingredient.order_item)
+      respond_to do |format|
+        format.html { redirect_to order_item_path(@customized_ingredient.order_item) }
+        format.js { render partial: "error" }
+      end
     end
   end
 end
