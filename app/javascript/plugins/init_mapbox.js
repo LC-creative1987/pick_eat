96e2a111241f4930851b181582e75cd7436e46d7
Laura Ciciulla <ciciullalaura@gmail.com>
2021-03-16 14:25:29 +0400

    I improved design and implemented windows

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,9 +1,9 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import places from 'places.js';
 
 const initMapbox = () => {
-
   const mapElement = document.getElementById('map');
 
   const fitMapToMarkers = (map, markers) => {
@@ -12,39 +12,39 @@ const initMapbox = () => {
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   };
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
+  const addMarkersToMap = (map, markers) => {
+    markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
 
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);      // Create a HTML element for your custom marker
+    });
+
+    fitMapToMarkers(map, markers)
+  }
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 
-    const map = new mapboxgl.Map({
+    // Generate the Map
+    const map = new mapboxgl.Map ({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
 
-    const markers = JSON.parse(mapElement.dataset.markers)
-    console.log(markers)
-    // console.log(markers)
-    markers.forEach((marker) => {
-      // const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-      // Create a HTML element for your custom marker
-      // const element = document.createElement('div');
-      // element.className = 'marker';
-      // element.style.backgroundImage = `url('${marker.image_url}')`;
-      // element.style.backgroundSize = 'contain';
-      // element.style.width = '25px';
-      // element.style.height = '25px';
+    // Add markers and fit them to the map
+    const markers = JSON.parse(mapElement.dataset.markers);
+    addMarkersToMap(map, markers);
 
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
-        // .setPopup(popup)
-    });
 
-    fitMapToMarkers(map, markers)
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
                                       mapboxgl: mapboxgl }));
+
   }
 
 };
 
 export { initMapbox };
+
