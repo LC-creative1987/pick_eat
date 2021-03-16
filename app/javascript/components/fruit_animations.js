c63d48e9d5a1a5d8bb5f4586ac9f8fd7a9e07c6f
Laura Ciciulla <ciciullalaura@gmail.com>
2021-03-16 18:28:48 +0400

    Navbar and Mapbox fixed

--- app/javascript/components/fruit_animations.js
+++ app/javascript/components/fruit_animations.js
@@ -2,15 +2,14 @@ const fruitAnimations = () => {
   const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
   // Select the veggie
 
-  const fruit = document.querySelector(".fruit")
+  const fruits = document.querySelectorAll(".fruit")
   // Change the position of the veggie, randomly
-
-  if (fruit) {
-
+  fruits.forEach((fruit) => {
     // Change the position of the veggie, randomly
     let fruitX = 15;
     let fruitY = 30;
-
+    fruit.style.right = `${fruitX}%`;
+    fruit.style.bottom = `${fruitY}%`;
     setInterval(() => {
 
       fruitX = getRandom(15, 30);
@@ -19,7 +18,8 @@ const fruitAnimations = () => {
       fruit.style.right = `${fruitX}%`;
       fruit.style.bottom = `${fruitY}%`;
     }, 2000)
-  }
+
+  })
 }
 
 export { fruitAnimations };
